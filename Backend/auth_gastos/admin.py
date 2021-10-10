ecd687f36bc900c7cd9f9f4f3d1f6d7f75db95c8
Anna <annagabriela2940@gmail.com>
2021-10-10 16:52:02 -0500

    Se crea la tabla/modelo Credit

---
+++ Backend/auth_gastos/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models.credit import Credit
+
+# Register your models here.
+admin.site.register(Credit)
---
+++ Backend/auth_gastos/models/credit.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Credit(models.Model):
+    cred_ID = models.AutoField(primary_key = True)
+    cred_Amount = models.FloatField()
+    cred_Description = 	models.TextField(max_length = 100, null = True)
+    cred_User = models.TextField(choices =(
+        ("M", "Mamá"),
+        ("S", "Sofia"),
+        ("D", "Daniela"),
+        ("G", "Gabriela")
+    ))
+    cred_Installment = models.IntegerField()
+    cred_Date = models.DateField()
